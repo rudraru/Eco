f0cace8d4b6d92d68cd26a36a83b76bd0291c0a7
Rudra Kharel <aayushkharelpunk@gmail.com>
2023-09-18 12:29:02 +0545

    Ghpages

--- src/Component/Page/Signup.jsx
+++ src/Component/Page/Signup.jsx
@@ -1,7 +1,10 @@
+// Signup.js
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import createUser from '../api/Api';
 import './Signup.css';
+import BackgroundImage from './Videobackground';
 
 const Signup = () => {
   const [users, setUsers] = useState([]);
@@ -54,38 +57,30 @@ const Signup = () => {
   };
 
   return (
-    <div className="signup-container">
-      <h1>Signup</h1>
 
-      {error && <p className="error">{error}</p>}
+      <div className="signup-container">
+        <h1>Signup</h1>
 
-      {success && (
-        <div className="success-popup">
+        {error && <p className="error">{error}</p>}
 
-          <p>Signup successful! Redirecting to LogIn page ...</p>
+        {success && (
+          <div className="success-popup">
+            <p>Signup successful! Redirecting to LogIn page ...</p>
+            {/* You may want to replace this text with an actual redirect or link */}
+            <p>Signup successful! Redirecting to home page...</p>
+          </div>
+        )}
 
-          <p>Signup successful! Redirecting to home page...</p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" value={name} onChange={event => setName(event.target.value)} required />
+          <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} required />
+          <input type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} required />
+          <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={event => setConfirmPassword(event.target.value)} required />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
 
-        </div>
-      )}
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name" value={name} onChange={event => setName(event.target.value) } required />
-        <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} required />
-        <input type="password" placeholder="Password" value={password} onChange={event => setPassword(event.target.value)} required/>
-        <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={event => setConfirmPassword(event.target.value)} required/>
-        <button type="submit">Submit</button>
-      </form>
-
-      {/* <h2>Users:</h2>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.name} - {user.email}</li>
-        ))}
-      </ul> */}
-    </div>
   );
 };
 
 export default Signup;
-
