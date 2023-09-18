e813124530667a39308cce8b9ad497de8fe859f9
Rudra Kharel <aayushkharelpunk@gmail.com>
2023-09-18 13:04:31 +0545

    .

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Eco"> {/* Set this to your repository name */}
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </React.StrictMode>,
